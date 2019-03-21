9698e09085f93e2b27ee17dbc9ad2c3e7b4ea539
Aanand Kainth <akainth015@gmail.com>
2019-03-20 17:34:53 -0700

    Disconnecting from the robot disables fields instead of deleting them

--- js/values.js
+++ js/values.js
@@ -46,58 +46,62 @@ function findContainerForKey(parts) {
 
 const timeouts = {};
 
-NetworkTables.addGlobalListener((key, value, isNew) => {
-    const keyButAnArray = key.split("/"), name = keyButAnArray.pop(), id = NetworkTables.keyToId(key);
+function subscribeToNetworkTables() {
+    return NetworkTables.addGlobalListener((key, value, isNew) => {
+        const keyButAnArray = key.split("/"), name = keyButAnArray.pop(), id = NetworkTables.keyToId(key);
 
-    if (isNew) {
-        const input = createElement("input", {
-            checked: value,
-            disabled: typeof value === "object",
-            id,
-            step: typeof value === "number" ? "any" : undefined,
-            type: {
-                boolean: "checkbox",
-                number: "number"
-            }[typeof value],
-            value: value,
-        }, "entry", {
-            run(e) {
-                const newValue = e.target.type === "checkbox" ? e.target.checked : (e.target.type === "number" ? parseFloat(e.target.value) : e.target.value);
-                NetworkTables.putValue(key, newValue);
-            },
-            type: "change"
-        });
+        if (isNew) {
+            const input = createElement("input", {
+                checked: value,
+                disabled: typeof value === "object",
+                id,
+                step: typeof value === "number" ? "any" : undefined,
+                type: {
+                    boolean: "checkbox",
+                    number: "number"
+                }[typeof value],
+                value: value,
+            }, "entry", {
+                run(e) {
+                    const newValue = e.target.type === "checkbox" ? e.target.checked : (e.target.type === "number" ? parseFloat(e.target.value) : e.target.value);
+                    NetworkTables.putValue(key, newValue);
+                },
+                type: "change"
+            });
 
-        const label = createElement("label", {
-            htmlFor: id,
-            id: `${id}-label`,
-            textContent: name
-        }, "entry", input);
+            const label = createElement("label", {
+                htmlFor: id,
+                id: `${id}-label`,
+                textContent: name
+            }, "entry", input);
 
-        const entry = createElement("div", {}, "entry", label);
+            const entry = createElement("div", {}, "entry", label);
 
-        const container = findContainerForKey(keyButAnArray);
-        container.append(entry);
-    } else {
-        const input = document.getElementById(id);
-        if (input !== document.activeElement) {
-            if (typeof value === "boolean") {
-                input.checked = value;
-            } else {
-                input.value = value;
+            const container = findContainerForKey(keyButAnArray);
+            container.append(entry);
+        } else {
+            const input = document.getElementById(id);
+            if (input !== document.activeElement) {
+                if (typeof value === "boolean") {
+                    input.checked = value;
+                } else {
+                    input.value = value;
+                }
+
+                const label = document.getElementById(`${id}-label`);
+                label.classList.add("changed");
+
+                clearTimeout(timeouts[id]);
+                timeouts[id] = setTimeout(() => {
+                    label.classList.remove("changed");
+                }, 1000);
             }
-
-            const label = document.getElementById(`${id}-label`);
-            label.classList.add("changed");
-
-            clearTimeout(timeouts[id]);
-            timeouts[id] = setTimeout(() => {
-                label.classList.remove("changed");
-            }, 1000);
         }
-    }
-    if (value === Infinity) console.debug(key, value);
-}, true);
+        if (value === Infinity) console.debug(key, value);
+    }, true);
+}
+
+let unsub = subscribeToNetworkTables();
 
 document.getElementById("download-nt-config").addEventListener("click", e => {
     window.open("/networktables/backup")
@@ -132,6 +136,13 @@ function download(filename, text) {
 
 NetworkTables.addRobotConnectionListener(connected => {
     if (connected === false) {
+        console.info("Disconnected from robot, disabling inputs");
+        unsub();
+        document.querySelectorAll("#values input").forEach(it => {
+            it.disabled = true;
+        });
+    } else{
         while (values.firstChild) values.removeChild(values.firstChild);
+        unsub = subscribeToNetworkTables();
     }
 });
